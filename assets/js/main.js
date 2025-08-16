db1e58cb723921c2d08789d0ba52a5f403c062e8
Ashish Tyagi <ashishtyagi10@gmail.com>
2025-08-16 19:34:32 -0400

    Update on the website

--- assets/js/main.js
+++ assets/js/main.js
@@ -55,20 +55,7 @@ console.log('%c Proudly Crafted with ZiOn.', 'background: #222; color: #bada55')
         }
 
         buildHomeSection(homeSection);
-        navbarAnimation(navbar, homeSection, navHeight);
-        navbarSubmenu(width);
-        hoverDropdown(width, mobileTest);
-
-        $(window).resize(function() {
-            var width = Math.max($(window).width(), window.innerWidth);
-            buildHomeSection(homeSection);
-            hoverDropdown(width, mobileTest);
-        });
-
-        $(window).scroll(function() {
-            effectsHomeSection(homeSection, this);
-            navbarAnimation(navbar, homeSection, navHeight);
-        });
+        
 
         /* ---------------------------------------------- /*
          * Set sections backgrounds
@@ -153,75 +140,13 @@ console.log('%c Proudly Crafted with ZiOn.', 'background: #222; color: #bada55')
          * Transparent navbar animation
          /* ---------------------------------------------- */
 
-        function navbarAnimation(navbar, homeSection, navHeight) {
-            var topScroll = $(window).scrollTop();
-            if (navbar.length > 0 && homeSection.length > 0) {
-                if(topScroll >= navHeight) {
-                    navbar.removeClass('navbar-transparent');
-                } else {
-                    navbar.addClass('navbar-transparent');
-                }
-            }
-        }
+        
 
         /* ---------------------------------------------- /*
          * Navbar submenu
          /* ---------------------------------------------- */
 
-        function navbarSubmenu(width) {
-            if (width > 767) {
-                $('.navbar-custom .navbar-nav > li.dropdown').hover(function() {
-                    var MenuLeftOffset  = $('.dropdown-menu', $(this)).offset().left;
-                    var Menu1LevelWidth = $('.dropdown-menu', $(this)).width();
-                    if (width - MenuLeftOffset < Menu1LevelWidth * 2) {
-                        $(this).children('.dropdown-menu').addClass('leftauto');
-                    } else {
-                        $(this).children('.dropdown-menu').removeClass('leftauto');
-                    }
-                    if ($('.dropdown', $(this)).length > 0) {
-                        var Menu2LevelWidth = $('.dropdown-menu', $(this)).width();
-                        if (width - MenuLeftOffset - Menu1LevelWidth < Menu2LevelWidth) {
-                            $(this).children('.dropdown-menu').addClass('left-side');
-                        } else {
-                            $(this).children('.dropdown-menu').removeClass('left-side');
-                        }
-                    }
-                });
-            }
-        }
-
-        /* ---------------------------------------------- /*
-         * Navbar hover dropdown on desctop
-         /* ---------------------------------------------- */
-
-        function hoverDropdown(width, mobileTest) {
-            if ((width > 767) && (mobileTest !== true)) {
-                $('.navbar-custom .navbar-nav > li.dropdown, .navbar-custom li.dropdown > ul > li.dropdown').removeClass('open');
-                var delay = 0;
-                var setTimeoutConst;
-                $('.navbar-custom .navbar-nav > li.dropdown, .navbar-custom li.dropdown > ul > li.dropdown').hover(function() {
-                        var $this = $(this);
-                        setTimeoutConst = setTimeout(function() {
-                            $this.addClass('open');
-                            $this.find('.dropdown-toggle').addClass('disabled');
-                        }, delay);
-                    },
-                    function() {
-                        clearTimeout(setTimeoutConst);
-                        $(this).removeClass('open');
-                        $(this).find('.dropdown-toggle').removeClass('disabled');
-                    });
-            } else {
-                $('.navbar-custom .navbar-nav > li.dropdown, .navbar-custom li.dropdown > ul > li.dropdown').unbind('mouseenter mouseleave');
-                $('.navbar-custom [data-toggle=dropdown]').not('.binded').addClass('binded').on('click', function(event) {
-                    event.preventDefault();
-                    event.stopPropagation();
-                    $(this).parent().siblings().removeClass('open');
-                    $(this).parent().siblings().find('[data-toggle=dropdown]').parent().removeClass('open');
-                    $(this).parent().toggleClass('open');
-                });
-            }
-        }
+        
 
         /* ---------------------------------------------- /*
          * Navbar collapse on click
